d662b17272167b03226af49da45b43ffc8958b55
lahari <laharireddy2923@gmail.com>
2025-09-22 16:16:20 +0530

    predefined

--- predefined.js
+++ predefined.js
@@ -1,3 +1,16 @@
-var a = prompt("enter value a is");
-var b = prompt("enter value b is");
-document.write(alert(a+b));
+const a=Number(prompt("enter a value"));
+const b=Number(prompt("enter a value"));
+document.writeln("you entered a= ",a," b=",b);
+
+function add(){
+alert(a+b);
+}
+function sub(){
+ alert(a-b);
+}
+function div(){
+    alert(a/b);
+}
+function mul(){
+    alert(a*b);
+}
