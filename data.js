244f56c4de40eab0897f5782933741e61628a77f
lahari <laharireddy2923@gmail.com>
2025-09-22 11:02:12 +0530

    datatype

---
+++ data.js
@@ -0,0 +1,31 @@
+//comment -
+//commit used to understand the code by the other developer
+//data types
+//1.primitive 2.non-primitive,
+//primitive-Number,String,Boolean,Null,Undefined
+//Non-primitive-Array,Object
+//typeof()-know the type of data
+//this,windows- commands to display 
+
+
+
+a = 7897;
+console.log(typeof a);
+name = "lahari";
+console.log(typeof name);
+c = true;
+console.log(typeof c);
+data = null;
+console.log(typeof data);
+d = undefined;
+console.log(typeof d);
+
+//non primitive
+array = [56,"lahari",true, null];
+console.log(typeof array);
+obj = {
+    name :"lahari",
+    age : "20",
+    dept :"cse",
+};
+console.log(typeof obj);
