7ecb98ed22567a622f9d3c6ced977dfd79376cde
laharireddy2923 <laharireddy2923@gmail.com>
2025-09-23 14:45:40 +0530

    Update function.js

--- function.js
+++ function.js
@@ -1,6 +1,36 @@
-//function -
-//code reusabity
+
+//function
+//code resuability
+function add(){
+    var a=57;
+    var b=78;
+    // document.write(a+b);
+    console.log(a+b);
+    
+
+}
+add();
+
+//way 2 of using functions(Es6)
+const sub=()=>{
+    // function sub(){
+    var a=57;
+    var b=78;
+    // document.write(a+b);
+    console.log(a-b);
+}
+// }
+sub();
+
+//Es7
+const mul=(a,b)=>console.log(a*b);
+mul(2,4);
 
 
-
-function add()
+let name="sruthi"
+	function demo(){
+	    let name="sru"
+	    document.write('hello my name is' $(name));
+	}
+document.write("hello,my name is" $(name));
+	demo();
